14abea502e3f88d1aa6570e275a43ab9f3472ede
IvanAKuzhelelev <kuzhelevdesign@gmail.com>
2021-01-08 19:37:06 +0300

    teapot included

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send("Hello! Hello! Hello!");
 });
 
+app.get("/pot", (req, res) => {
+  res.status(418).send("418, I'm a teapot");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
