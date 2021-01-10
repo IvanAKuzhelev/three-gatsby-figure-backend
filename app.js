4effb31ba53f23aa3e1517452e02fd548ea44571
IvanAKuzhelelev <kuzhelevdesign@gmail.com>
2021-01-10 22:21:09 +0300

    added cors and js based calculation

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+const drawValuesCalculator = require("./drawValuesCalculator");
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello! Hello! Hello!");
 });
 
-app.get("/pot", (req, res) => {
-  res.status(418).send("418, I'm a teapot");
+app.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.post("/", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.send(drawValuesCalculator.fnc(req.body.x, req.body.y, req.body.z));
 });
---
+++ drawValuesCalculator.js
@@ -0,0 +1,53 @@
+function fnc(x, y, z) {
+  const drawValues = {
+    // prettier-ignore
+    vertices: [
+    //front
+    //0
+    0, 0, 0,
+    //1
+    x, 0, 0,
+    //2
+    x, y, 0,
+    //3
+    0, y, 0,
+    //back
+    //4
+    0, 0, z,
+    //5
+    0, y, z,
+    //6
+    x, y, z,
+    //7
+    x, 0, z,
+  ],
+    // prettier-ignore
+    indices: [
+    // //front
+    2, 1, 0,
+    0, 3, 2,
+    // //bottom
+    4, 0, 1,
+    1, 7, 4,
+    // //top
+    6, 2, 3,
+    3, 5, 6,
+    //left-side
+    3, 0, 4,
+    4, 5, 3,
+    // //right-side
+    6, 7, 1,
+    1, 2, 6,
+    // //back
+    5, 4, 7,
+    7, 6, 5
+  ],
+    camera: {
+      x: 9 * x,
+      y: 3 * y,
+      z: 2 * z,
+    },
+  };
+  return drawValues;
+}
+exports.fnc = fnc;
